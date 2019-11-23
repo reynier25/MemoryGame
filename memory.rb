aa7da5cbfbe38a14c5b2e5a8ed53d5ce15886e0a
reynier25 <fsoto@uchicago.edu>
2019-11-23 12:42:12 -0800

    add move logic, player method names

--- memory.rb
+++ memory.rb
@@ -35,4 +35,39 @@ class MemoryGame
     pos
   end
 
+  def make_guess(pos)
+    revealed_value = board.reveal(pos)
+    player.receive_revealed_card(pos, revealed_value)
+    board.render
+
+    compare_guess(pos)
+
+    sleep(1)
+    board.render
+  end
+
+  def match?(pos1, pos2)
+    board[pos1] == board[pos2]
+  end
+
+  def play
+    until board.won?
+      board.render
+      pos = get_player_input
+      make_guess(pos)
+    end
+
+    puts "Congratulations, you win!"
+  end
+
+  def valid_pos?(pos)
+    pos.is_a?(Array) &&
+      pos.count == 2 &&
+      pos.all? { |x| x.between?(0, board.size - 1) }
+  end
+
+  private
+
+  attr_accessor :previous_guess
+  attr_reader :board
 end
