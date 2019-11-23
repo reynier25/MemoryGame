ee837d4aa906ae3dd51c13967fa495933f309a5e
reynier25 <fsoto@uchicago.edu>
2019-11-22 18:22:45 -0800

    game logic

---
+++ memory.rb
@@ -0,0 +1,38 @@
+require_relative "board"
+
+class MemoryGame
+  attr_reader :player
+
+  def initialize(player, size = 4)
+    @board = Board.new(size)
+    @previous_guess = nil
+    @player = player
+  end
+
+  def compare_guess(new_guess)
+    if previous_guess
+      if match?(previous_guess, new_guess)
+        player.receive_match(previous_guess, new_guess)
+      else
+        puts "Try again."
+        [previous_guess, new_guess].each { |pos| board.hide(pos) }
+      end
+      self.previous_guess = nil
+      player.previous_guess = nil
+    else
+      self.previous_guess = new_guess
+      player.previous_guess = new_guess
+    end
+  end
+
+  def get_player_input
+    pos = nil
+
+    until pos && valid_pos?(pos)
+      pos = player.get_input
+    end
+
+    pos
+  end
+
+end
